850df420215af6057abf938fd16db142bb1dbe01
Ulsonic <vagolev@velog.ru>
2021-02-27 21:30:33 +0300

    little clear

--- stm32_snail/manta_contr/src/inc/board.h
+++
@@ -1,9 +0,0 @@
-#ifndef __BOARD_H__
-#define __BOARD_H__
-////======================================
-#include "manta_contr_brd.h"
-
-////======================================
-extern void tst_task( void *pvParameters );
-
-#endif
