189dfec92880ddd945fa3bc07414cd9d7096c1c6
vovag <vagolev@mail.ru>
2024-07-30 00:55:02 +0300

    change win

--- win/qt_win_snail/csv/csv_dlg.cpp
+++ win/qt_win_snail/csv/csv_dlg.cpp
@@ -39,7 +39,7 @@ void csv_dlg::SlotOpenFile()
     csvModel->setColumnCount(CSV_NUM_COL);
  ///   csvModel->setHorizontalHeaderLabels(QStringList() << "Марка" << "Модель" << "Цена");
     ui.tableView->setModel(csvModel); // Устанавливаем модель в таблицу
-    csvModel->setHorizontalHeaderLabels(QStringList() << "Марка" << "Модель" << "Цена");
+  ////  csvModel->setHorizontalHeaderLabels(QStringList() << "Марка" << "Модель" << "Цена");
 
 
 QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"), "",
@@ -52,13 +52,20 @@ QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"), "",
             return;
         }
      QTextStream in(&file);
+     QStringList head_list;
+     QString head_line = in.readLine();
+     for (QString h_item : head_line.split(",")) {
+         head_list<< h_item;
+     }
+     csvModel->setHorizontalHeaderLabels(head_list);
+
      while (!in.atEnd())
         {
             // ... построчно
             QString line = in.readLine();
             // Добавляем в модель по строке с элементами
             QList<QStandardItem*> standardItemsList;
-            for (QString item : line.split(";")) {
+            for (QString item : line.split(",")) {
                 standardItemsList.append(new QStandardItem(item));
             }
             csvModel->insertRow(csvModel->rowCount(), standardItemsList);
